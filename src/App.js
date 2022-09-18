a9ba7ad72788f87f86afcd492ccdf55b737058ca
ghaniz <ghanizahid@hotmail.com>
2022-09-18 10:27:26 -0400

    inital setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
 
 function App() {
+  const [ideas, setIdeas] = useState([
+    "Movie about a guy who gets bit by a radioactive mole rat",
+    "Romcom about two developers over Zoom",
+    "Horror film where AWS goes down for 24 hours"
+  ]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Welcome, Super Awesome Movie Monster Think Tank!</h1>
+
+      <br />
+      
+      <h1>Submit your ideas down below:</h1>
+
+
+    </main>
   );
 }
 
 export default App;
+
